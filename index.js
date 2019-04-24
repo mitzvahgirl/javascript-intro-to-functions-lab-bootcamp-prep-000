a954a4c558b8613caf15a571872681e2b0f771a2
Amy Fogelman <abfnj1@gmail.com>
2019-04-24 12:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ function whisper(string) {
   return string.toLowerCase();
 }
   
-function logShout(string) calls console.log() its one argument in all caps:
-logWhisper(string) calls console.log() its one argument in all lowercase:
-sayHiToGrandma(string) returns "I can't hear you!" if `string` is lowercase
+function logShout(string) {
+  console.log() its one argument in all caps:
+  
+  
+function logWhisper(string) {
+  calls console.log() its one argument in all lowercase:
+  
+function sayHiToGrandma(string) {
+  return string ("I can't hear you!") if `string` is lowercase
  sayHiToGrandma(string) returns "YES INDEED!" if `string` is uppercase:
 
  sayHiToGrandma(string) returns "I love you, too." if `string` is "I love you, Grandma."`:
