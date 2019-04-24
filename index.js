aa3a4dbf1aaaae51935e34682822236d91ad9d01
Amy Fogelman <abfnj1@gmail.com>
2019-04-24 12:27:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ function shout(string) {
 logShout(string) calls console.log() its one argument in all caps:
 logWhisper(string) calls console.log() its one argument in all lowercase:
 sayHiToGrandma(string) returns "I can't hear you!" if `string` is lowercase
+
+ sayHiToGrandma(string) returns "I love you, too." if `string` is "I love you, Grandma."`:
