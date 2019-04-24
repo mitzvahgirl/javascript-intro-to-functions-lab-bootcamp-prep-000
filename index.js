5530f6c30a4c8ffd06699c23ac2c7b4c241c8aac
Amy Fogelman <abfnj1@gmail.com>
2019-04-24 12:30:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
+
 function whisper(string) {
-  return string.toLowerCase() 
+  return string.toLowerCase();
 }
   
 logShout(string) calls console.log() its one argument in all caps:
